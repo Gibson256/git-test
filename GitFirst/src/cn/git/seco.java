b780fd34e8cf095f0cc684f59e732462876d6c1a
Gibson <beijing.429026987@163.com>
2019-03-14 17:28:20 +0800

    添加一个second2

---
+++ GitFirst/src/cn/git/seco.java
@@ -0,0 +1,17 @@
+package cn.git;
+
+public class seco {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+    public void test() {
+        int i=10;
+        i+=5;
+        System.out.println(i);
+    }
+
+    public void tog(){
+        System.out.println("okokok");
+    }
+}
