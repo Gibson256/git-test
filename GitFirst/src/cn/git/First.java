3cda91f0f08bd15cdbd860a0b9935fbfd7773aaa
Gibson <beijing.429026987@163.com>
2019-03-14 17:05:07 +0800

    修改内容

--- GitFirst/src/cn/git/First.java
+++ GitFirst/src/cn/git/First.java
@@ -3,10 +3,11 @@ package cn.git;
 public class First {
     public static void main(String[] args) {
         System.out.println("hello git");
-
     }
 
     public void test() {
-        System.out.println("byebye");
+        int i=10;
+        i+=5;
+        System.out.println(i);
     }
 }
